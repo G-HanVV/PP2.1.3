d1b4a4bdc2ff83188efcc0d77f1b83b3941c1993
G-HanVV <62301594+G-HanVV@users.noreply.github.com>
2024-08-31 10:08:50 +0300

    Hibernate 3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,37 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+    // реализуйте настройку соединения с БД
     private static Connection connection;
+    private static SessionFactory sessionFactory;
+    private static Session session;
+
+    public static Session getSession(){
+        if (session == null || !session.isOpen()) {
+            sessionFactory = new Configuration()
+                    .configure()
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+            session = sessionFactory.getCurrentSession();
+        }
+        return session;
+    }
+
+    public static void closeSession(){
+        if (session != null) {
+            session.close();
+            sessionFactory.close();
+        }
+    }
 
 
     public static Connection getConnection() throws SQLException {
